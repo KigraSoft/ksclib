7f8a213c43a1a9258768fd5588e4fca30f02a0a7
Stephen R. Kifer <srk@kigra.com>
2026-01-24 06:11:15 -0500

    initial arena code

---
+++ ksclib.c
@@ -0,0 +1,24 @@
+/*
+  ksclib.c
+
+  KigraSoft C Library is a set of utility functions that I use in my
+  other programs.  It is shared so that anyone else that finds it
+  useful may also take advantage of it.
+
+  Principles:
+  - It is intended to be included directly for compilation as a single translation unit
+  - It uses its internal memory arena structures where possible
+  - All functions will be named in a 'kcl_xxx_' pattern where xxx is a code for the different main library 'modules'.
+  - It will probably be C23 standard compliant
+  - ???
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "src/ksclib-arena.c"
+
+
